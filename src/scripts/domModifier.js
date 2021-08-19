d1edcdf9283de9fb3fc8c534486e6665f92ebf57
coderxpb <borapankaj901@gmail.com>
2021-08-19 11:05:59 +0530

    Fix domPage

--- src/scripts/domModifier.js
+++ src/scripts/domModifier.js
@@ -1,13 +1,10 @@
-const pageDom = (function () {
-  const location = document.getElementById('weather-location');
-  // const search = Document.getElementById('weather-search');
-  // const icon = Document.getElementById('weather-icon');
-  const desc = document.getElementById('weather-desc');
+const pageDom = {};
+pageDom.location = document.getElementById('weather-location');
+pageDom.desc = document.getElementById('weather-desc');
 
-  function changePageDom(weatherData) {
-    location.textContent = weatherData.location;
-    desc.textContent = weatherData.desc;
-  }
-}());
+pageDom.changePageDom = function (weatherData) {
+  pageDom.location.textContent = weatherData.location;
+  pageDom.desc.textContent = weatherData.desc;
+};
 
 export default pageDom;
