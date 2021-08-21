348c01c3c81109535a1b74677c51a047608b0331
coderxpb <borapankaj901@gmail.com>
2021-08-21 09:49:11 +0530

    Change background and icon according to main weather

--- src/scripts/domModifier.js
+++ src/scripts/domModifier.js
@@ -1,19 +1,41 @@
-import weatherI from '../images/amcharts_weathericons/animated/weather.svg';
+import cloud from '../images/amcharts_weathericons/animated/cloudy.svg';
+import clearD from '../images/amcharts_weathericons/animated/day.svg';
+import clearN from '../images/amcharts_weathericons/animated/night.svg';
+import rain from '../images/amcharts_weathericons/animated/rainy-7.svg';
+import snow from '../images/amcharts_weathericons/animated/snowy-6.svg';
+import thundestorm from '../images/amcharts_weathericons/animated/thunder.svg';
+import weather from '../images/amcharts_weathericons/animated/weather.svg';
+import drizzle from '../images/amcharts_weathericons/animated/rainy-5.svg';
 
 const pageDom = {};
+pageDom.body = document.querySelector('body');
 pageDom.location = document.getElementById('weather-location');
 pageDom.icon = document.getElementById('weather-icon');
 pageDom.desc = document.getElementById('weather-desc');
 pageDom.tempF = document.getElementById('weather-temp');
 pageDom.windhumidity = document.getElementById('weather-windhumidity');
-
+pageDom.icons = {
+  clouds: cloud,
+  clear: clearD,
+  rain,
+  snow,
+  thundestorm,
+  weather,
+  drizzle,
+};
 pageDom.changePageDom = function (weatherData) {
-  const weatherJ = weatherI;
+  // const weatherJ = weatherI;
+  if (Object.keys(pageDom.icons).includes(weatherData.main)) {
+    pageDom.body.classList = weatherData.main;
+    pageDom.icon.src = pageDom.icons[weatherData.main];
+    console.log(weather);
+  } else {
+    pageDom.body.classList = 'snow';
+    pageDom.icon.src = pageDom.icons.weather;
+  }
   pageDom.location.textContent = weatherData.location;
-  pageDom.icon.src = weatherJ;
-  console.log(weatherJ);
-  pageDom.desc.textContent = weatherData.desc;
 
+  pageDom.desc.textContent = weatherData.desc;
   pageDom.tempF.textContent = weatherData.tempF;
   pageDom.windhumidity.textContent = `${weatherData.windspeed} ${weatherData.humidity}`;
 };
--- src/scripts/weatherAPICalls.js
+++ src/scripts/weatherAPICalls.js
@@ -3,7 +3,7 @@ import pageDom from './domModifier';
 function getUsefulWeatherData(location, data) {
   const newData = {};
   newData.location = location;
-  newData.main = data.weather[0].main;
+  newData.main = data.weather[0].main.toLowerCase();
   newData.desc = data.weather[0].description;
   newData.tempF = data.main.temp;
   newData.humidity = data.main.humidity;
@@ -14,14 +14,15 @@ function getUsefulWeatherData(location, data) {
 export default async function getWeatherData() {
   try {
     // const inputLocation = prompt('Enter your location: ');
-    const inputLocation = 'Bangalore';
+    const inputLocation = 'Egypt';
     console.log(inputLocation);
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputLocation}&appid=ab0f54a57cc1277f45830d3aebbcf28e`, { mode: 'cors' });
     const data = await response.json();
     const usefulData = getUsefulWeatherData(inputLocation, data);
-    console.log(usefulData);
+    console.log(data);
     pageDom.changePageDom(usefulData);
   } catch (err) {
+    console.log('enter a valid location');
     console.log(err);
   }
 }
